1708f827c02955ed4925d9a10497c1464e6209c0
haurog <36535774+haurog@users.noreply.github.com>
2023-05-09 12:20:25 +0200

    Added a date picker. Still has some quirks.

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,27 +1,32 @@
 import React, { useContext, useState, useEffect } from 'react';
 import SkimRewards from './SkimRewards';
 
+import Datepicker from "react-tailwindcss-datepicker";
+
+
 export default function Calculator() {
 
   const [size, setSize] = useState(0)  // in MB
   const [impressions, setImpressions] = useState(0)
   const [gridEmissionPerkWhInkg, setGridEmissionPerkWhInkg] = useState(0);
 
-  const handleSizeChange = event => {
-    setSize(event.target.value);
-    // console.log("Size: ", event.target.value, size)
-  };
+  const rocketPoolStartDate = new Date("2021-11-7")  // started on Nov 8th, but need to set 1 day earlier for calendar to work
+  const today = new Date();
 
-  const handleImpressionsChange = event => {
-    setImpressions(event.target.value);
-    // console.log("Impressions: ", event.target.value, impressions)
-  };
+  const [value, setValue] = useState({
+    startDate: new Date(),
+    endDate: new Date().setMonth(11)
+  });
 
-  const handleGridEmissionChange = event => {
-    setGridEmissionPerkWhInkg(event);
-    // console.log("Grid emission: ", event, gridEmissionPerkWhInkg)
-  };
+  const handleValueChange = (newValue) => {
+    console.log("newValue:", newValue);
+    if (Date.parse(newValue.endDate) > Date.parse(today)) {
+      newValue.endDate = today;
+    }
+    setValue(newValue);
+  }
 
+  console.log("value: ", value)
 
   return (<div className="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
     <div className="bg-white sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg">
@@ -30,32 +35,14 @@ export default function Calculator() {
         <div className="mt-6">
           <div className="space-y-6">
             <div>
-
-              <label htmlFor="startDate" className="sr-only">
-                Start date:
-              </label>
-              <input
-                onChange={handleSizeChange}
-                type="number"
-                name="start"
-                id="start"
-                placeholder="Start Date"
-                required
-                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
-              />
-            </div>
-            <div>
-              <label htmlFor="impressions" className="sr-only">
-                End Date:
-              </label>
-              <input
-                onChange={handleImpressionsChange}
-                type="number"
-                name="impressions"
-                id="end"
-                placeholder="End Date"
-                required
-                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+              <Datepicker
+                primaryColor={"orange"}
+                minDate={rocketPoolStartDate}
+                maxDate={today}
+                // startWeekOn="mon"
+                value={value}
+                onChange={handleValueChange}
+                showShortcuts={true}
               />
             </div>
             <div>
