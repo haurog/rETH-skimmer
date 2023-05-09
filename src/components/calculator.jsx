26de3a5fff860c67ffb5f6df2aed2bf8cae501d3
haurog <36535774+haurog@users.noreply.github.com>
2023-05-09 13:51:38 +0200

    Fix the date picker quirks. Works now

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -12,21 +12,28 @@ export default function Calculator() {
 
   const rocketPoolStartDate = new Date("2021-11-7")  // started on Nov 8th, but need to set 1 day earlier for calendar to work
   const today = new Date();
+  const startOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  var tempLastMonth = new Date;  // Will be set to a date in the last month
+  tempLastMonth.setDate(startOfTheMonth.getDate() - 10);
+  const startOfLastMonth = new Date(tempLastMonth.getFullYear(), tempLastMonth.getMonth(), 1);
 
-  const [value, setValue] = useState({
-    startDate: new Date(),
-    endDate: new Date().setMonth(11)
+  const [dateRange, setDateRange] = useState({
+    startDate: null,
+    endDate: null
   });
 
-  const handleValueChange = (newValue) => {
+
+  const handleDateRangeChange = (newValue) => {
     console.log("newValue:", newValue);
     if (Date.parse(newValue.endDate) > Date.parse(today)) {
       newValue.endDate = today;
     }
-    setValue(newValue);
+    setDateRange(newValue);
   }
 
-  console.log("value: ", value)
+  console.log("dateRange: ", dateRange)
+  console.log("startOfTheMonth: ", startOfTheMonth);
+  console.log("startOfLastMonth: ", startOfLastMonth);
 
   return (<div className="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
     <div className="bg-white sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg">
@@ -37,12 +44,20 @@ export default function Calculator() {
             <div>
               <Datepicker
                 primaryColor={"orange"}
+                showShortcuts={true}
+                showFooter={true}
                 minDate={rocketPoolStartDate}
                 maxDate={today}
-                // startWeekOn="mon"
-                value={value}
-                onChange={handleValueChange}
-                showShortcuts={true}
+                startFrom={startOfLastMonth}
+                configs={{
+                  shortcuts: {
+                    past: period => `Last ${period} days`,
+                    currentMonth: "Current Month",
+                    pastMonth: "Past Month"
+                  }
+                }}
+                value={dateRange}
+                onChange={handleDateRangeChange}
               />
             </div>
             <div>
