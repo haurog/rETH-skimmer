561039a45df5aff0a953f0e15262282f72b5fdbe
haurog <36535774+haurog@users.noreply.github.com>
2023-06-01 11:10:05 +0200

    Further tweak the footer location

--- src/components/RETHAPYFooter.jsx
+++ src/components/RETHAPYFooter.jsx
@@ -55,7 +55,7 @@ export default function RETHAPYFooter(props) {
 
     return (
         <div>
-            <div className="py-24 sm:py-32">
+            <div className="py-12 sm:py-14">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:max-w-none">
                         <div className="text-center">
