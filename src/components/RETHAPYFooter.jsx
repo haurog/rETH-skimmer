27306642207235e74e03872985ae59da7e569b71
haurog <36535774+haurog@users.noreply.github.com>
2023-06-01 10:40:09 +0200

    moved the footer upwards

--- src/components/RETHAPYFooter.jsx
+++ src/components/RETHAPYFooter.jsx
@@ -59,9 +59,9 @@ export default function RETHAPYFooter(props) {
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:max-w-none">
                         <div className="text-center">
-                            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">APY of rETH over different time spans:</h2>
+                            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">rETH APY over different time spans:</h2>
                         </div>
-                        <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+                        <dl className="mt-4 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
                             {stats.map((stat) => (
                                 <div key={stat.id} className="flex flex-col bg-white/5 p-8">
                                     <dt className="text-sm font-semibold leading-6 text-gray-300">{stat.name}</dt>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -63,7 +63,7 @@ export default function Example() {
           <rect width={364} height={384} fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)" />
         </svg>
       </div>
-      <div className="relative pt-6 pb-16 sm:pb-24">
+      <div className="relative pt-6 pb-16 sm:pb-12">
         <Popover>
           <nav
             className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
@@ -75,7 +75,7 @@ export default function Example() {
           </nav>
         </Popover>
 
-        <main className="mt-16 sm:mt-24">
+        <main className="mt-24">
           <div className="mx-auto max-w-7xl">
             <div className="lg:grid lg:grid-cols-12 lg:gap-8">
               <div className="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:flex lg:items-center lg:text-left">
