3f23ad9472c83917d274e400481040914047e97b
haurog <36535774+haurog@users.noreply.github.com>
2023-06-05 09:19:15 +0200

    moved date range element to its own component

---
+++ src/components/DateRangeInput.jsx
@@ -0,0 +1,40 @@
+import Datepicker from "react-tailwindcss-datepicker";
+
+
+
+
+export default function DateRangeInput(props) {
+
+  const rocketPoolStartDate = new Date("2021-11-08T00:00:00Z")  // started on Nov 9th at midnight, but need to set 1 day earlier to be able to select the start date in the calendar
+  const today = new Date();
+  const startOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  var tempLastMonth = new Date;  // Will be set to a date in the last month
+  tempLastMonth.setDate(startOfTheMonth.getDate() - 10);  // temporary date to be sure to be in the last month
+  const startOfLastMonth = new Date(tempLastMonth.getFullYear(), tempLastMonth.getMonth(), 1);
+
+  return (
+    <div>
+      <label className="block text-sm leading-6 text-gray-900">
+        Date range over which to calculate the rETH yield.
+      </label>
+      <Datepicker
+        primaryColor={"orange"}
+        showShortcuts={true}
+        showFooter={true}
+        minDate={rocketPoolStartDate}
+        maxDate={today}
+        startFrom={startOfLastMonth}
+        configs={{
+          shortcuts: {
+            past: period => `Last ${period} days`,
+            currentMonth: "Current Month",
+            pastMonth: "Past Month"
+          }
+        }}
+        value={props.dateRange}
+        onChange={props.handleDateRangeChange}
+      />
+    </div>
+  )
+
+}
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -4,8 +4,7 @@ import { ethers } from 'ethers';
 
 import SkimRewards from './SkimRewards';
 import DateETHToggler  from './dateETHToggler'
-
-import Datepicker from "react-tailwindcss-datepicker";
+import DateRangeInput  from './DateRangeInput'
 
 import { findRETHRatioByDate, calcRateIncrease, calcEquivalentAPY } from '../helper/RETHCalculations'
 
@@ -17,12 +16,8 @@ const rETH_CONTRACT_ADDRESS = addressesToken.rETH;
 
 
 export default function Calculator(props) {
-  const rocketPoolStartDate = new Date("2021-11-08T00:00:00Z")  // started on Nov 9th at midnight, but need to set 1 day earlier to be able to select the start date in the calendar
+
   const today = new Date();
-  const startOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-  var tempLastMonth = new Date;  // Will be set to a date in the last month
-  tempLastMonth.setDate(startOfTheMonth.getDate() - 10);  // temporary date to be sure to be in the last month
-  const startOfLastMonth = new Date(tempLastMonth.getFullYear(), tempLastMonth.getMonth(), 1);
 
   const [dateRange, setDateRange] = useState({
     startDate: null,
@@ -119,28 +114,7 @@ export default function Calculator(props) {
                 className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
               />
             </div>
-            <div>
-              <label className="block text-sm leading-6 text-gray-900">
-                Date range over which to calculate the rETH yield.
-              </label>
-              <Datepicker
-                primaryColor={"orange"}
-                showShortcuts={true}
-                showFooter={true}
-                minDate={rocketPoolStartDate}
-                maxDate={today}
-                startFrom={startOfLastMonth}
-                configs={{
-                  shortcuts: {
-                    past: period => `Last ${period} days`,
-                    currentMonth: "Current Month",
-                    pastMonth: "Past Month"
-                  }
-                }}
-                value={dateRange}
-                onChange={handleDateRangeChange}
-              />
-            </div>
+            <DateRangeInput dateRange={dateRange} handleDateRangeChange={handleDateRangeChange}/>
             <div>
               <h2>equivalent ETH: {equivalentETH.toPrecision(3)} rETH</h2>
             </div>
