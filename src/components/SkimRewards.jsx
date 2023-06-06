ba857af2e9e83e26b54b200fcf77b0f5fcc640c5
haurog <36535774+haurog@users.noreply.github.com>
2023-06-06 09:48:15 +0200

    improve toast message

--- src/components/SkimRewards.jsx
+++ src/components/SkimRewards.jsx
@@ -36,7 +36,7 @@ export default function SkimRewards(props) {
       console.log('Success', data)
       const CustomToastWithLink = () => (
         <div>
-          <a href={"https://polygonscan.com/tx/" + data.transactionHash}>Carbon emission offset</a>
+          <a href={"https://etherscan.io/tx/" + data.transactionHash}>Carbon emission offset</a>
         </div>
       );
       const notify = () => {
