a7548fba261daa2554edba3d6b1059d68f2981ac
haurog <36535774+haurog@users.noreply.github.com>
2023-06-20 11:27:13 +0200

    disable input fields and add tooltips if rETH ratio is not loaded yet.

--- src/components/DateRangeInput.jsx
+++ src/components/DateRangeInput.jsx
@@ -1,31 +1,44 @@
 import Datepicker from "react-tailwindcss-datepicker";
+import { Tooltip } from 'react-tooltip'
 import { importantDates } from '../helper/ImportantDates';
 
 export default function DateRangeInput(props) {
+
+  let tooltipText = ""
+  if (props.inputDisabled) {
+    tooltipText = "The rETH exchange rates have not been loaded yet. This might take up to 15 seconds."
+  }
 
   return (
     <div>
       <label className="block text-sm leading-6 text-gray-900">
         Date range over which to calculate the rETH yield:
       </label>
-      <Datepicker
-        primaryColor={"orange"}
-        showShortcuts={true}
-        showFooter={false}
-        startWeekOn="mon"
-        minDate={importantDates.rocketPoolStartDateForCalendar}
-        maxDate={importantDates.today}
-        startFrom={importantDates.startOfLastMonth}
-        configs={{
-          shortcuts: {
-            past: period => `Last ${period} days`,
-            currentMonth: "Current Month",
-            pastMonth: "Past Month"
-          }
-        }}
-        value={props.dateRange}
-        onChange={props.handleDateRangeChange}
-      />
+      <div
+        data-tooltip-id="input-field-tooltip"
+        data-tooltip-content={tooltipText}
+        data-tooltip-place="bottom">
+        <Datepicker
+          primaryColor={"orange"}
+          showShortcuts={true}
+          showFooter={false}
+          startWeekOn="mon"
+          minDate={importantDates.rocketPoolStartDateForCalendar}
+          maxDate={importantDates.today}
+          startFrom={importantDates.startOfLastMonth}
+          configs={{
+            shortcuts: {
+              past: period => `Last ${period} days`,
+              currentMonth: "Current Month",
+              pastMonth: "Past Month"
+            }
+          }}
+          value={props.dateRange}
+          onChange={props.handleDateRangeChange}
+          disabled={props.inputDisabled}
+        />
+      </div>
+      <Tooltip id="input-field-tooltip" />
     </div>
   )
 
--- src/components/ETHInputField.jsx
+++ src/components/ETHInputField.jsx
@@ -1,22 +1,35 @@
+import { Tooltip } from 'react-tooltip'
 
 
 export default function EthInputField(props) {
+
+  let tooltipText = ""
+  if (props.inputDisabled) {
+    tooltipText = "The rETH exchange rates have not been loaded yet.\n This might take up to 15 seconds."
+  }
 
   return (
     <div>
       <label htmlFor="ETH" className="block text-sm leading-6 text-gray-900">
         ETH amount that should remain after skimming:
       </label>
-      <input
-        value={props.ETHToRemain}
-        onChange={props.handleETHToRemainChange}
-        type="number"
-        name="ETH"
-        id="ETH"
-        placeholder="ETH"
-        required
-        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm py-2.5"
-      />
+      <div
+        data-tooltip-id="input-field-tooltip"
+        data-tooltip-content={tooltipText}
+        data-tooltip-place="bottom">
+        <input
+          value={props.ETHToRemain}
+          onChange={props.handleETHToRemainChange}
+          type="number"
+          name="ETH"
+          id="ETH"
+          placeholder="ETH"
+          required
+          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm py-2.5"
+          disabled={props.inputDisabled}
+        />
+        <Tooltip id="input-field-tooltip" />
+      </div>
     </div>
   )
 
