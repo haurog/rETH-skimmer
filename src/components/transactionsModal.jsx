f12f7c0880eb939886bfc5e29401e3b1edb6ca2b
haurog <36535774+haurog@users.noreply.github.com>
2023-07-04 20:55:48 +0200

    Add background blur for transaction modal to mach connect modal

--- src/components/transactionsModal.jsx
+++ src/components/transactionsModal.jsx
@@ -27,7 +27,7 @@ export default function TransactionsModal() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              <div className="backdrop-blur-sm fixed inset-0 bg-gray-900 bg-opacity-50 transition-opacity" />
             </Transition.Child>
 
             <div className="fixed inset-0 z-10">
