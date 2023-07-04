7cbb7305b23a154423baec6d15a282c55a157e8b
haurog <36535774+haurog@users.noreply.github.com>
2023-07-04 20:43:33 +0200

    autoformat

--- src/components/transactionsModal.jsx
+++ src/components/transactionsModal.jsx
@@ -15,54 +15,54 @@ export default function TransactionsModal() {
           Show Previous Transactions
         </button>
       </div>
-     <div>
-       <Transition.Root show={open} as={Fragment}>
-         <Dialog as="div" className="relative z-10" onClose={setOpen}>
-           <Transition.Child
-             as={Fragment}
-             enter="ease-out duration-300"
-             enterFrom="opacity-0"
-             enterTo="opacity-100"
-             leave="ease-in duration-200"
-             leaveFrom="opacity-100"
-             leaveTo="opacity-0"
-           >
-             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-           </Transition.Child>
+      <div>
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
 
-           <div className="fixed inset-0 z-10">
-             <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-               <Transition.Child
-                 as={Fragment}
-                 enter="ease-out duration-300"
-                 enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                 enterTo="opacity-100 translate-y-0 sm:scale-100"
-                 leave="ease-in duration-200"
-                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-               >
-                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-screen-xl sm:p-6">
-                   <div>
-                     <div className="mt-3 text-center sm:mt-5">
-                       <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                         Previous Skim Transactions:
-                       </Dialog.Title>
-                       <div className="mt-2">
-                         <TransactionsTable />
-                       </div>
-                       <hr />
-                       <div className="mt-2 text-sm text-gray-400">
-                         Previous rETH skim transactions show up here. These transactions are stored locally in your browser. If you use a different browser, or delete the local browser storage they are not showing up anymore.
-                       </div>
-                     </div>
-                   </div>
-                 </Dialog.Panel>
-               </Transition.Child>
-             </div>
-           </div>
-         </Dialog>
-       </Transition.Root>
-     </div>
+            <div className="fixed inset-0 z-10">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-screen-xl sm:p-6">
+                    <div>
+                      <div className="mt-3 text-center sm:mt-5">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          Previous Skim Transactions:
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <TransactionsTable />
+                        </div>
+                        <hr />
+                        <div className="mt-2 text-sm text-gray-400">
+                          Previous rETH skim transactions show up here. These transactions are stored locally in your browser. If you use a different browser, or delete the local browser storage they are not showing up anymore.
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      </div>
     </div >
   )
 }
